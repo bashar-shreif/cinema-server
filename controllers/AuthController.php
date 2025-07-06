685212b07490556c5635453152fc826c0ce6f6a9
Bashar Shreif <basharchreif70@gmail.com>
2025-07-07 01:51:08 +0300

    Adding auth and user controllers as well as user model

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,19 +1,54 @@
 <?php
 require("Controller.php");
+require("../models/User.php");
 
 class AuthController extends Controller
 {
     public function login()
     {
+        if (empty($_POST)) {
+            echo ResponseService::error_response("Wrong credentials");
+            return;
+        }
+        $user = User::getByEmail($_POST["email"]);
+        if (!$user) {
+            echo ResponseService::error_response("Email doesn't exist");
+            return;
+        }
+        if (!password_verify($_POST["password"], $user->getPassword())) {
+            echo ResponseService::error_response("Wrong Password");
+            return;
+        }
+        $id = $user->getId();
+        echo ResponseService::success_response($id);
+        return;
+
     }
     public function logout()
     {
     }
-    public function register(string $username, string $name, string $email, string $password, string $phone)
+    public function register()
     {
-        
+        if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["username"]) && isset($_POST["age"]) && isset($_POST["phone"]) && isset($_POST["name"])) {
+            $data = [
+                "username" => $_POST["username"],
+                "name" => $_POST["name"],
+                "age" => (int) $_POST["age"],
+                "email" => $_POST["email"],
+                "password" => password_hash($_POST["password"], PASSWORD_DEFAULT),
+                "phone" => $_POST["phone"]
+            ];
+        } else {
+            echo ResponseService::error_response("Missing info");
+            return;
+        }
+        if (!User::exists($_POST["email"])) {
+            echo ResponseService::error_response("Email is already used");
+            return;
+        }
+        $user = User::create($data);
+        echo ResponseService::success_response($user);
+        return;
     }
-    public function checkAuthentication()
-    {
-    }
+
 }
--- routes/routes.php
+++ routes/routes.php
@@ -2,6 +2,12 @@
 
 require("Route.php");
 
+//Routes to the movie controller.
 Route::Route($apis, '/movies', 'MovieController::getAll()');
-Route::Route($apis,'','');
+Route::Route($apis,'/delete_movie','MovieController::deleteAll()');
+Route::Route($apis,'/add_movie','MovieController::create()');
+Route::Route($apis,'/edit_movie','MovieController::edit()');
+Route::Route($apis,'/new_movies','MovieController::getUpcomingMovies()');
+Route::Route($apis,'/current_movies','MovieController::getCurrentMovies()');
+
 ?>
